115574ce5aadca73c22369596439aa378e6080da
Nicolas Unger <nicounger94@gmail.com>
2018-03-27 15:32:28 -0300

    Fixed several errors.

--- src/ns_test.py
+++ src/ns_test.py
@@ -5,7 +5,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-import pprint as pp
+import copy
 
 path = "/home/nunger/tesis/codigo/data/spectral_lines.txt"
 data = np.loadtxt(path, delimiter=" ")
@@ -16,6 +16,7 @@ signal = data[:,1]
 Tmin = 0.01
 Tmax = 100
 
+#np.random.seed(1234)
 # --------------------- Function definitions -----------------------------
 def model(T, nu, nu0=37, sigmaL=2):
     """" Model 1 for the spectral lines model. """
@@ -52,12 +53,39 @@ def SampleDist(cdf, x, N):
         idx = (np.abs(cdf-samples[i])).argmin()
         dist[i] = x[idx]
     return dist
+
+class ActiveObj:
+    def __init__(self, T, cdf, xdata, ydata):
+        self.param = 0
+        self.Lhood = 0
+        self.xdata = xdata
+        self.ydata = ydata
+        self.cdf = cdf
+        self.T = T
+        self.wt = 0
+
+    def Sample(self):
+        """ Samples the object. """
+        self.param = SampleDist(cdf, self.T, 1)
+        self.Lhood = Likelihood(self.param, self.ydata, self.xdata)
+
+    def Evolve(self, Lconstraint):
+        """ Evolves the Object to find a new sample given the
+        Likelihood constraint. """
+        while 1:
+            tsample = SampleDist(cdf, T, 1)
+            tL = Likelihood(tsample, self.ydata, self.xdata)
+            if tL > Lconstraint:
+                self.param = tsample
+                self.Lhood = tL
+                break
+
 # --------------------------------------------------------------------------
 
-npts = 2000
+npts = 5000
 T = np.linspace(Tmin,Tmax,npts)
 pdf = [Likelihood(t, signal, channel) for t in T]
-print "Tiene que dar: {}".format(np.trapz(pdf,T))
+print "Analytic integration: {}\n".format(np.trapz(pdf,T))
 
 cdf = CDF(pdf, T)
 cdf /= max(cdf) # Normalization of the CDF
@@ -69,61 +97,67 @@ cdf /= max(cdf) # Normalization of the CDF
 
 # ------------------- Nested Sampling algorithm ----------------------------
 # Definition of variables and objects
-N = 10 # Number of active objects
+N = 1000 # Number of active objects
 N_MAX = 10000 # Maximum samples
 Obj = [] # List of active objects
 Samples = [] # All Samples
 Lstar = 0 # Current Likelihhod constraint
 w = 1 - np.exp(-1./N) # First width in prior mass
+xi = [] # Prior mass
 H = 0.0 # Information
-Z = sys.float_info.min #-sys.float_info.max * sys.float_info.epsilon # Evidence, initially 0
+Z = sys.float_info.min # Evidence, initially 0
 Znew = Z # Updated Evidence
 nest = 0 # Current iteration of the Nested Sampling loop
-end = 2.0 # End condition for loop
+end = 3.0 # End condition for loop
 
 # Initialization of first objects
 for i in range(N):
-    tsample = SampleDist(cdf, T, 1) # Sample 1 point from pdf
-    Obj.append([tsample, Likelihood(tsample, signal, channel)]) # Set objects
+    Obj.append(ActiveObj(T, cdf, channel, signal)) # Creates an Active Object
+    Obj[i].Sample() # Samples it
 
 # Begin Nested Sampling loop
-while nest <= 50: #end * N * H:
+while nest <= end * N * H:
     # Search for worst Likelihood within the active objects
     worst = 0
     for i in range(N):
-        if(Obj[i][1] < Obj[worst][1]):
+        if(Obj[i].Lhood < Obj[worst].Lhood):
             worst = i
-    currZ = w * Obj[worst][1] # Weight of object
+    currZ = w * Obj[worst].Lhood # Weight of object
+    Obj[worst].wt = currZ
 
     # Update Evidence and Information
     Znew = Z + currZ
-    H = (currZ / Znew) * np.log(Obj[worst][1]) + (Z/Znew)*(H+np.log(Z)) - np.log(Znew)
+    H = (currZ / Znew) * np.log(Obj[worst].Lhood) + (Z/Znew)*(H+np.log(Z)) - np.log(Znew)
     Z = Znew
-    print "Z = {} \t H = {:.3f} \t n = {}".format(Z, H, nest)
+    #print "Z = {} \t H = {:.3f} \t L = {} \t n = {}".format(Z, H, Obj[worst].Lhood, nest)
 
-    Samples.append(Obj[worst])
+    # Save all chosen samples
+    Samples.append(copy.deepcopy(Obj[worst]))
 
     #Kill worst object in favour of a new object
-    Lstar = Obj[worst][1] # Update Likelihood constraint
-    if N!=1: # Keep it if N=1
-        while 1:
-            tsample = SampleDist(cdf, T, 1)
-            tL = Likelihood(tsample, signal, channel)
-            if tL > Lstar:
-                Obj[worst] = [tsample, tL, 0]
-                break
+    Lstar = Obj[worst].Lhood # Update Likelihood constraint
+    Obj[worst].Evolve(Lstar) # Evolve the old sample for a new one
 
+    xi.append(np.exp(-float(nest)/N))
     nest += 1
+    w = np.exp(-float(nest)/N) - np.exp(-float(nest+1)/N) # Shrink weight size
+
     # Break loop if nest exeeds the maximum value
     if nest >= N_MAX:
+        print "Loop exceeded maximum iteration number of {}".format(N_MAX)
         break
-    w = np.exp(-float(nest/N)) - np.exp(-float(nest+1)/N) # Shrink weight size
 
-print "\nFinal evidende: {}".format(Z)
+print "Iterations: {}".format(nest)
+print "Final evidende: {}".format(Z)
 
+# Plotting of solution
+lvector = [obj.Lhood for obj in Samples]
 
+plt.figure()
+plt.plot(xi, lvector)
+
+plt.show()
 # --------------------------------------------------------------------------
-
 
 
 # # Plot of data
