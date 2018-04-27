62bba1058207b3728609c1a3d077eee96b464479
Nicolas Unger <nicounger94@gmail.com>
2018-04-27 13:32:42 -0300

    Started plotting Distribution of evidencees

--- src/plots/plot_estimator_evolution.py
+++ src/plots/plot_estimator_evolution.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 from scipy.stats import iqr
+from scipy.optimize import curve_fit
 
 # Importing data
 path = "/home/nunger/tesis/graficos/nested_sampling_test/"
@@ -35,15 +36,30 @@ N = np.append(N, [2500, 3000, 4000, 5000])
 
 # Distribution of N
 results500 = np.loadtxt(path + "iter500/results_300_501_200.txt")
+N500 = [300, 500]
+# Gaussian model
+def gauss(x, *p):
+    A, mu, sigma = p
+    return A*np.exp(-(x-mu)**2/(2.*sigma**2))
 
-# Use Freedman Draconis rule to choose bin size
-IQR = iqr(results500[0]) # Interquartile range
+p0 = [4, 1.0e-38, 1.0e-38]
+
+# Use of Freedman–Diaconis rule to choose bin size
+IQR = iqr(results500, axis=1) # Interquartile range
 binSize = 2 * (IQR / len(results500[0])**(1./3))
-nBins = int((np.max(results500[0]) - np.min(results500[0])) / binSize)
+nBins = np.divide(np.max(results500, axis=1) - np.min(results500, axis=1), binSize)
+nBins = nBins.astype(int)
 # Plot
-plt.figure(2)
-plt.hist(results500[0], nBins)
+f, ax = plt.subplots(2, 1, sharex=True)
+for i in range(2):
+    hist, bin_edges, patches = ax[i].hist(results500[i], bins='fd') #nBins[i])
+    bin_centres = (bin_edges[:-1] + bin_edges[1:])/2
+    coeff, var_matrix = curve_fit(gauss, bin_centres, hist, p0=p0)
+    x = np.linspace(min(results500[i]), max(results500[i]), 200)
+    hist_fit = gauss(x, *coeff)   
+    ax[i].plot(x, hist_fit, label='Fitted data')
+    ax[i].set_title("Distribution of results for N = {}".format(N500[i]))
+    
 plt.xlabel("Evidence")
 plt.ylabel("Ocurrences")
-
 plt.show()
