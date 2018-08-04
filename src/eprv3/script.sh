3b718f43adef1ee2cf54339d5dd27b35e2ea9d75
Nicolas Unger <nicounger94@gmail.com>
2018-08-04 13:08:46 -0300

    More results

--- src/eprv3/script.sh
+++ src/eprv3/script.sh
@@ -1,8 +1,8 @@
 ulimit -s unlimited
 for dfile in {1..3}
 do
-    for i in {1..3}
+    for i in {1..2}
     do
-        python ppc_eprv3.py -n 3 -nlive 25 -dfile $dfile --clust
+        python ppc_eprv3.py -n 3 -dfile $dfile --clust --narrow
     done
 done
