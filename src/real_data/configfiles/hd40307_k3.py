c42615f0db076df8130d6cbfdde491f237af4e16
Nicolas Unger <nicounger94@gmail.com>
2018-08-31 13:09:59 +0200

    two and three planet models

---
+++ src/real_data/configfiles/hd40307_k3.py
@@ -0,0 +1,73 @@
+target = 'HD40307'
+runid = 'k1'
+
+import os
+import pickle
+import numpy as np
+
+datapath = os.path.join(os.getenv('HOME'), 'tesis/codigo/data/')
+
+# -------------------
+# DATA
+# -------------------
+datadict = {
+    'rvrd':
+    {'datafile':  os.path.join(datapath, target+'/HD40307_HARPS03(DRS-3-5).rdb'),
+     'instrument': 'HARPS03',
+     'mask': 'G2',
+     'type': 'RV',
+     'jittermodel': 'constant'
+     },
+}
+
+# ---------------------
+#        PLANETS
+# --------------------
+fp1dict = {'k1': [2.76, 2, ['ModJeffreys', 1., 999.], 0.5],
+           'period': [42.0615, 2, ['Jeffreys', 1.0, 1e4], 0.13],
+           'ecc': [0.1, 1, ['Uniform', 0., 1.]],
+           'omega': [0.1, 1, ['Uniform', 0., 2*np.pi]],
+           'ma0': [0.1, 1, ['Uniform', 0., 2*np.pi]],
+           'epoch': [278.019, 0]
+           }
+
+fp2dict = {'k1': [2.51, 2, ['ModJeffreys', 1., 999.], 0.5],
+           'period': [42.1173, 2, ['Jeffreys', 1.0, 1e4], 0.13],
+           'secos': [0.1, 1, ['Uniform', -1., 1.]],
+           'sesin': [0.1, 1, ['Uniform', -1., 1.]],
+           # 'ecc': [0.1, 1, ['Uniform', 0., 1.]],
+           # 'omega' : [0.1, 1, ['Uniform', 0., 2*np.pi]],
+           'ma0': [0.1, 1, ['Uniform', 0., 2*np.pi]],
+           'epoch': [278.019, 0]
+           }
+
+fp3dict = {'k1': [2.51, 2, ['ModJeffreys', 1., 999.], 0.5],
+           'period': [42.1173, 2, ['Jeffreys', 1.0, 1e4], 0.13],
+           'secos': [0.1, 1, ['Uniform', -1., 1.]],
+           'sesin': [0.1, 1, ['Uniform', -1., 1.]],
+           # 'ecc': [0.1, 1, ['Uniform', 0., 1.]],
+           # 'omega' : [0.1, 1, ['Uniform', 0., 2*np.pi]],
+           'ma0': [0.1, 1, ['Uniform', 0., 2*np.pi]],
+           'epoch': [278.019, 0]
+           }
+
+driftdict = {'v0': [0.0, 1, ['Uniform', -1000., 1000.]],
+             'lin': [0.0, 0, ['Uniform', -10., 10.]],
+             'qua': [0.0, 0],
+             'cub': [0.0, 0],
+             'tref': [278.019, 0],
+             'unitconstant': [1.0, 0],
+             }
+
+rvrddict = {'offset': [31334.45, 0, ['Uniform', -32000, 32000]],
+            'jitter': [0.0, 1, ['ModJeffreys', 1.0, 99.0]],
+            }
+
+objectdict = {'rvrd': rvrddict,
+              'drift1': driftdict,
+              'planet1': fp1dict,
+              'planet2': fp2dict,
+              'planet3': fp3dict
+              }
+
+configdicts = [objectdict, datadict]
